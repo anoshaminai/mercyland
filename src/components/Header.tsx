9defaa50af69c90c31262829a7d6ef4691d0d8b8
Anosha Minai <anoshaminai@gmail.com>
2025-04-25 14:45:43 -0500

    header using header.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/Header.css';
 
 const Header: React.FC = () => {
   return (
-    <nav className="flex justify-between items-center px-8 py-4">
-      <div className="flex gap-8">
-        <NavLink to="/" className="text-[#FF0000]">INFO</NavLink>
-        <NavLink to="/listen" className="text-[#FF0000]">LISTEN</NavLink>
-        <NavLink to="/watch" className="text-[#FF0000]">WATCH 'TERMITES'</NavLink>
-        {/* <NavLink to="/shows" className="text-[#FF0000]">SHOWS</NavLink>
-        <NavLink to="/shop" className="text-[#FF0000]">SHOP</NavLink> */}
-      </div>
-      <div>
-        <span className="text-[#FF0000]">VOID: <span className="text-white">ON</span> OFF</span>
-      </div>
-    </nav>
+    <header className="header">
+      <nav className="navigation">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? 'active' : ''}
+        >
+          INFO
+        </NavLink>
+        <NavLink 
+          to="/listen" 
+          className={({ isActive }) => isActive ? 'active' : ''}
+        >
+          LISTEN
+        </NavLink>
+        <NavLink 
+          to="/watch" 
+          className={({ isActive }) => isActive ? 'active' : ''}
+        >
+          WATCH 'TERMITES'
+        </NavLink>
+        {/* <NavLink to="/shows">SHOWS</NavLink>
+        <NavLink to="/shop">SHOP</NavLink> */}
+      </nav>
+    </header>
   );
 };
 
