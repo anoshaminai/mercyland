928788ceacd9d125a652b83f1786154d97d23b7b
Anosha Minai <anoshaminai@gmail.com>
2025-05-13 15:47:58 -0500

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,28 +5,30 @@ import '../styles/Header.css';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <nav className="navigation">
-        <NavLink 
-          to="/" 
-          className={({ isActive }) => isActive ? 'active' : ''}
-        >
-          INFO
-        </NavLink>
-        <NavLink 
-          to="/listen" 
-          className={({ isActive }) => isActive ? 'active' : ''}
-        >
-          LISTEN
-        </NavLink>
-        <NavLink 
-          to="/watch" 
-          className={({ isActive }) => isActive ? 'active' : ''}
-        >
-          WATCH 'TERMITES'
-        </NavLink>
-        {/* <NavLink to="/shows">SHOWS</NavLink>
-        <NavLink to="/shop">SHOP</NavLink> */}
-      </nav>
+      <div className="max-w-4xl mx-auto">
+        <nav className="navigation">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => isActive ? 'active' : ''}
+          >
+            INFO
+          </NavLink>
+          <NavLink 
+            to="/listen" 
+            className={({ isActive }) => isActive ? 'active' : ''}
+          >
+            LISTEN
+          </NavLink>
+          <NavLink 
+            to="/watch" 
+            className={({ isActive }) => isActive ? 'active' : ''}
+          >
+            WATCH 'TERMITES'
+          </NavLink>
+          {/* <NavLink to="/shows">SHOWS</NavLink>
+          <NavLink to="/shop">SHOP</NavLink> */}
+        </nav>
+      </div>
     </header>
   );
 };
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import logo from '../assets/images/temp_logo.png';
 const Logo: React.FC = () => {
   return (
-    <h1 className="text-6xl text-[#4A7F3F] mb-12">
-      Mercy Land
-    </h1>
+    <div>
+      <h1 className="text-6xl text-[#4A7F3F] mb-12 justify-center">
+        Mercy Land
+      </h1>
+      <img src={logo} alt="Mercy Land Logo" className="w-full h-auto" />
+    </div>
   );
 };
 
