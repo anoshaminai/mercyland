a7ee344b51fd160b08c07700bc7f6fecfa820480
Anosha Minai <anoshaminai@gmail.com>
2025-05-20 11:12:57 -0500

    format email bar to have blue background etc

--- src/components/EmailSignup.tsx
+++ src/components/EmailSignup.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const EmailSignup: React.FC = () => {
   return (
-    <div className="flex justify-center gap-2 items-center">
-      <span className="text-mercy-green font-primary">KEEP UP:</span>
+    <div className="flex justify-center items-center bg-mercy-blue border border-mercy-red">
+      <span className="text-mercy-green font-primary px-3 py-2 border-r border-mercy-red text-center min-w-[100px]">KEEP UP:</span>
       <input
         type="email"
-        className="bg-mercy-green text-mercy-white font-primary px-4 py-2 outline-none border-mercy-green"
+        className="bg-mercy-blue text-mercy-white font-primary px-4 py-2 outline-none border-r border-mercy-red w-96"
         placeholder="YOUR EMAIL"
       />
-      <button className="bg-mercy-green text-mercy-white font-primary px-4 py-2">
+      <button className="bg-mercy-blue text-mercy-green font-primary px-3 py-2 hover:text-mercy-white transition-colors min-w-[100px] text-center">
         SUBMIT
       </button>
     </div>
