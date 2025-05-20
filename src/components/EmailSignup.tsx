a75dc883e1b3793906025cfb1d336b885037c1d5
Anosha Minai <anoshaminai@gmail.com>
2025-05-20 10:44:52 -0500

    use themes to apply font + colors to header and email

--- src/components/EmailSignup.tsx
+++ src/components/EmailSignup.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 const EmailSignup: React.FC = () => {
   return (
     <div className="flex justify-center gap-2 items-center">
-      <span className="text-mercy-green font-medium">KEEP UP:</span>
+      <span className="text-mercy-green font-primary">KEEP UP:</span>
       <input
         type="email"
-        className="bg-mercy-green text-mercy-white px-4 py-2 outline-none border-mercy-green"
+        className="bg-mercy-green text-mercy-white font-primary px-4 py-2 outline-none border-mercy-green"
         placeholder="YOUR EMAIL"
       />
-      <button className="bg-mercy-green text-mercy-white px-4 py-2">
+      <button className="bg-mercy-green text-mercy-white font-primary px-4 py-2">
         SUBMIT
       </button>
     </div>
