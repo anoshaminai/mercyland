30dd4e6d5802034b79523c71a8d358b358a2e97f
Anosha Minai <anoshaminai@gmail.com>
2025-06-10 15:46:23 -0500

    added formspree functionality;

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,21 +15,6 @@ const Home: React.FC = () => {
         <div className="max-w-content mx-auto">
           <Logo />
           <EmailSignup />
-          <div className="mt-4">
-            <iframe 
-              id="laylo-drop-fd1Vn"
-              frameBorder="0"
-              scrolling="no"
-              allow="web-share"
-              allowTransparency={true}
-              style={{
-                width: '1px',
-                minWidth: '100%',
-                maxWidth: '1000px'
-              }}
-              src="https://embed.laylo.com?dropId=fd1Vn&color=FF2F00&minimal=false&theme=light"
-            />
-          </div>
         </div>
       </main>
     </div>
