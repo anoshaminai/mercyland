7c3200dc4075013b651f93544309eaebc1dd6325
Anosha Minai <anoshaminai@gmail.com>
2025-09-09 16:42:14 -0400

    add other songs

--- src/pages/ListenPage.tsx
+++ src/pages/ListenPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Home from '../components/common/Home';
 import kidA from '../assets/images/Kid A cover art.jpg';
+import getLost from '../assets/images/Get Lost cover art.jpg';
+import wtlgo from '../assets/images/wtlgo cover art.jpg';
+import rt42 from '../assets/images/Rt42 cover art.jpg';
 import Container from '../components/layout/Container';
 import Section from '../components/layout/Section';
 import ContentColumn from '../components/ui/ContentColumn';
@@ -11,36 +14,19 @@ const ListenPage: React.FC = () => {
     <>
       <Home />
       <Section background="bg-mercy-pink">
+        
         <Container className="gap-8">
+
+          {/* Kid A section */}
           <ContentColumn className="items-center">
             <h2 className="text-4xl text-mercy-black font-primary text-center">Kid A</h2>
             <img 
               src={kidA}
               alt="Kid A album cover" 
-              className="w-198 h-198 object-cover"
+              className="w-150 h-150 object-cover"
             />
           </ContentColumn>
-          
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
-            <ContentColumn>
-              <iframe 
-                src="https://open.spotify.com/embed/album/2VHtTgQDgzi3z4MewLHfSP?utm_source=generator" 
-                width="350px" 
-                height="352" 
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                loading="lazy"
-              />
-            </ContentColumn>
-            
-            <ContentColumn>
-              <iframe 
-                width="350px" 
-                height="350"  
-                allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2080693716&color=%23902722&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-              />
-            </ContentColumn>
-            
+
             <ContentColumn>
               <iframe 
                 style={{ border: 0, width: '350px', height: '350px' }}
@@ -50,22 +36,72 @@ const ListenPage: React.FC = () => {
                 <a href="https://thankgodformercyland.bandcamp.com/track/kid-a">Kid A by Mercy Land</a>
               </iframe>
             </ContentColumn>
+
+
+                    {/* Get Lost section */}
+          <ContentColumn className="items-center">
+            <h2 className="text-4xl text-mercy-black font-primary text-center">Get Lost!</h2>
+            <img 
+              src={getLost}
+              alt="Get Lost! album cover" 
+              className="w-150 h-150 object-cover"
+            />
+          </ContentColumn>
+         
             
-            {/* <ContentColumn>
+            <ContentColumn>
               <iframe 
-                allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
-                style={{ 
-                  width: '350px',
-                  height: '100%',
-                  overflow: 'hidden',
-                  borderRadius: '10px'
-                }}
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-                src="https://embed.music.apple.com/us/embed/song/kid-a/1805286177"
-              />
-            </ContentColumn> */}
-          </div>
+                style={{ border: 0, width: '350px', height: '350px' }}
+                src="https://bandcamp.com/EmbeddedPlayer/track=2691189257/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless
+              >
+                <a href="https://thankgodformercyland.bandcamp.com/track/get-lost">Get Lost! by Mercy Land</a>
+              </iframe>
+            </ContentColumn>
+
+            {/* WTLGO section */}
+          <ContentColumn className="items-center">
+            <h2 className="text-4xl text-mercy-black font-primary text-center">When the Lights</h2>
+            <img 
+              src={wtlgo}
+              alt="When the lights album cover" 
+              className="w-150 h-150 object-cover"
+            />
+          </ContentColumn>
+         
+
+            <ContentColumn>
+              <iframe 
+                style={{ border: 0, width: '350px', height: '350px' }}
+                src="https://bandcamp.com/EmbeddedPlayer/track=2373043130/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless
+              >
+                <a href="https://thankgodformercyland.bandcamp.com/track/when-the-lights">When the Lights by Mercy Land</a>
+              </iframe>
+            </ContentColumn>
+
+          {/* Rt42 section */}
+          <ContentColumn className="items-center">
+            <h2 className="text-4xl text-mercy-black font-primary text-center">Route 42</h2>
+            <img 
+              src={rt42}
+              alt="Rt 42 album cover" 
+              className="w-150 h-150 object-cover"
+            />
+          </ContentColumn>
+         
+            
+            <ContentColumn>
+              <iframe 
+                style={{ border: 0, width: '350px', height: '350px' }}
+                src="https://bandcamp.com/EmbeddedPlayer/track=2373043130/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless
+              >
+                <a href="https://thankgodformercyland.bandcamp.com/track/kid-a">Kid A by Mercy Land</a>
+              </iframe>
+            </ContentColumn>
         </Container>
+        
       </Section>
     </>
   );
