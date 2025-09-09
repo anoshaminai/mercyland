8d92128d8f87fe37e115b768d5f01ff6eb74eff1
Anosha Minai <anoshaminai@gmail.com>
2025-09-09 15:56:22 -0400

    convert info page to using ui components

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import Home from '../components/common/Home';
 import touchingImage from '../assets/images/touching.jpg';
+import GridRow from '../components/ui/GridRow';
+import Section from '../components/layout/Section';
+import ContentColumn from '../components/ui/ContentColumn';
 
 const InfoPage: React.FC = () => {
   return (
     <>
-      <Home />
-      <section className="min-h-screen bg-mercy-red pt-8 pb-16">
-        <div className="max-w-content mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-items-center">
-            <div className="space-y-4">
-              {/* <h2 className="text-4xl text-mercy-green font-primary text-center">About Us</h2> */}
-              <p className="text-mercy-black font-primary">
-                Thank God for Mercy Land
-              </p>
-            </div>
-            <img 
-              src={touchingImage}
-              alt="Band photo 1" 
-              className="w-full h-screen object-cover"
-            />
-          </div>
-        </div>
-      </section>
-    </>
+    <Home />
+  <Section background="bg-mercy-red">
+    <GridRow>
+      <ContentColumn className="space-y-4">
+        {/* <h2 className="text-4xl text-mercy-green font-primary text-center">About Us</h2> */}
+        <p className="text-mercy-black font-primary">
+          Thank God for Mercy Land
+        </p>
+      </ContentColumn>
+      <ContentColumn>
+        <img 
+          src={touchingImage}
+          alt="Band photo 1" 
+          className="w-full h-screen object-cover"
+        />
+      </ContentColumn>
+    </GridRow>
+  </Section>
+  </>
   );
 };
 
